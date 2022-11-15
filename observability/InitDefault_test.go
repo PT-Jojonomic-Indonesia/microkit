3b44ffb330b4e4562e3be4bb070dda302a73fb0a
jojoarief <arief@jojonomic.com>
2022-11-15 15:02:21 +0700

    logger stdout untuk otel
    logger stdout untuk otel

---
+++ observability/InitDefault_test.go
@@ -0,0 +1,41 @@
+package observability_test
+
+import (
+	"context"
+	"errors"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/PT-Jojonomic-Indonesia/microkit/sentry"
+	"github.com/PT-Jojonomic-Indonesia/microkit/tracer"
+	"github.com/joho/godotenv"
+	"go.opentelemetry.io/otel/attribute"
+)
+
+func TestInitDefault(t *testing.T) {
+	godotenv.Load()
+	os.Setenv("SERVICE_NAME", "observability")
+	os.Setenv("SERVICE_VERSION", "v1.0.0")
+
+	// observability.InitDefault()
+	// tracer.InitOtel(os.Getenv("JAEGER_URL"), os.Getenv("SERVICE_NAME"), os.Getenv("SERVICE_VERSION"), os.Getenv("ENVIRONMENT"))
+	// cfg := sentry.Config{
+	// 	Dsn:         os.Getenv("SENTRY_DSN"),
+	// 	Release:     os.Getenv("SERVICE_VERSION"),
+	// 	Environment: os.Getenv("ENVIRONMENT"),
+	// }
+	// sentry.Init(cfg)
+	// defer sentry.Close()
+
+	tracer.InitOtel(os.Getenv("JAEGER_URL"), os.Getenv("SERVICE_NAME"), os.Getenv("SERVICE_VERSION"), os.Getenv("ENVIRONMENT"))
+
+	_, span := tracer.Start(context.Background(), "init_test", "", "")
+	defer span.End()
+
+	span.SetAttributes(attribute.String("test", "test"))
+	sentry.CaptureError(errors.New("test error"))
+	time.Sleep(5 * time.Second)
+
+	tracer.InitOtel(os.Getenv("JAEGER_URL"), os.Getenv("SERVICE_NAME"), os.Getenv("SERVICE_VERSION"), os.Getenv("ENVIRONMENT"))
+}
