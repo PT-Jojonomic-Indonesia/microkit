305fcb8665bce068144183f29966fdbcea9b54ad
Ach. Vani Ardiansyah <achvani@refactory.id>
2022-11-23 22:24:29 +0700

    add custom validator for length of integer

--- validator/validate.go
+++ validator/validate.go
@@ -2,6 +2,7 @@ package validator
 
 import (
 	"context"
+	"strconv"
 
 	validator_lib "github.com/go-playground/validator/v10"
 )
@@ -10,6 +11,7 @@ var validate *validator_lib.Validate
 
 func Init() {
 	validate = validator_lib.New()
+	validate.RegisterValidation("int_lte", validateNumberOfDigit)
 }
 
 func Validate(ctx context.Context, data interface{}) error {
@@ -28,4 +30,24 @@ func GetErrors(err error) map[string]interface{} {
 		}
 	}
 	return mapErrors
+}
+
+func validateNumberOfDigit(fl validator_lib.FieldLevel) bool {
+	field := fl.Field()
+	param, err := strconv.Atoi(fl.Param())
+	if err != nil {
+		panic(err.Error())
+	}
+
+	v := field.Int()
+	if v < 0 {
+		panic("negative number")
+	}
+
+	n := 0
+	for ; v > 0; v /= 10 {
+		n += 1
+	}
+
+	return n <= param
 }
